5070511ee03232f19f319199258c42e45e1cec93
amfzn503 <amfzn503@msn.com>
2020-03-18 16:38:33 -0500

    started work on studio

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// TODO: add code here
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            const container = document.getElementById("container");
+            for (let index = 0; index < json.length; index++) {
+                let name = json[index].firstName + " " + json[index].lastName
+                let div = `
+            <div class="astronaut">
+                <h3>${name}</h3>
+                <ul>
+                    <li>Hours in space: ${json[index].hoursInSpace}</li>
+                    <li>Active: ${json[index].active}</li>
+                    <li>Skills: ${json[index].skills}</li>
+                </ul>
+
+                <img class="avatar" src= ${json[index].picture}>
+            </div>
+            `
+                container.innerHTML += div
+            }
+        });
+    });
+});
