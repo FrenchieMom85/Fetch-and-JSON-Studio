7eeed2f7e2913818fcdecde803f4e4126c86aad7
amfzn503 <amfzn503@msn.com>
2020-03-20 17:59:10 -0500

    Final studio

--- script.js
+++ script.js
@@ -4,20 +4,35 @@ window.addEventListener("load", function () {
             const container = document.getElementById("container");
             for (let index = 0; index < json.length; index++) {
                 let name = json[index].firstName + " " + json[index].lastName
+                let ifActive = json[index].active
+                json.sort(function (a, b) {
+                    return b.hoursInSpace - a.hoursInSpace;
+                });
                 let div = `
             <div class="astronaut">
+            <div class="bio">
                 <h3>${name}</h3>
                 <ul>
                     <li>Hours in space: ${json[index].hoursInSpace}</li>
-                    <li>Active: ${json[index].active}</li>
+                    <li id ="isActive">Active: ${ifActive}</li>
                     <li>Skills: ${json[index].skills}</li>
                 </ul>
-
+                </div>
                 <img class="avatar" src= ${json[index].picture}>
             </div>
             `
                 container.innerHTML += div
-            }
+                container.innerHTML += `
+                <div class="astronautCount">
+                    <h3>Total Astronauts = ${index + 1}</h3>
+                </div>
+                `
+                if (ifActive === "true"){
+                    isActive.style.color = "green"
+                }
+            }   
+
         });
     });
 });
+
